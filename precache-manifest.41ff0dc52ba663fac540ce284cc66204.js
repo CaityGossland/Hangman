27def22d2bd6d18c377a6555d7902094fb6e557b
Caity Gossland <caitygossland@gmail.com>
2022-02-10 11:08:56 -0800

    Updates

---
+++ precache-manifest.41ff0dc52ba663fac540ce284cc66204.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8dc85984d5c55389b5f8a39242aad7",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "d11310884530cb1acf92",
+    "url": "/Hangman/static/css/main.761d37cf.chunk.css"
+  },
+  {
+    "revision": "6547628edf715736d3b2",
+    "url": "/Hangman/static/js/2.1eb91280.chunk.js"
+  },
+  {
+    "revision": "d11310884530cb1acf92",
+    "url": "/Hangman/static/js/main.406f8294.chunk.js"
+  },
+  {
+    "revision": "f8832925578ad51d01a6",
+    "url": "/Hangman/static/js/runtime~main.9bccb078.js"
+  }
+]);
